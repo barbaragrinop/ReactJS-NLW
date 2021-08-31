cc3c9cf688608647a9f6c0bf0f8966f0a8b31ac9
Barbara Hellen <barbara.pereira@modalgr.com.br>
2021-08-31 18:01:52 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,69 @@
+import { useHistory } from 'react-router-dom';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss'
+import { Button } from '../components/Button';
+import { UseAuth } from '../hooks/UseAuth';
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
+
+
+export function Home(){
+    const history = useHistory();
+    const {signIniWithGoogle, user} = UseAuth();
+    const [roomCode, setRoomCode] = useState('');
+
+    async function handleCreateRoom(){
+        if(!user){
+            await signIniWithGoogle()
+        }
+       history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+        if(roomCode.trim() == ''){
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+        
+        if(!roomRef.exists()){
+            alert('Room doesnt exists');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`);
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"  />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as duvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <button className="create-room" onClick={handleCreateRoom}>
+                        <img src={googleIconImg} alt="Logo do google"  />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o codigo da sala" 
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
+                        />
+                        <Button type="submit" onClick={handleJoinRoom}>Entrar na sala</Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
